5fe293a0e2b682e628310d617e7e4a2e0b360436
cara <cyoon3232@gmail.com>
2026-08-01 23:57:39 -0700

    refactor types

---
+++ humscore/src/types.ts
@@ -0,0 +1,50 @@
+export type LivePitch = {
+  frequency: number,
+  note: string,
+  rawNote: string,
+  cents: number,
+  clarity: number,
+  volume: number
+}
+
+export type PitchFrame = {
+  time: number,
+  frequency: number,
+  rawNote: string,
+  correctedNote: string,
+  correctedMidi: number,
+  cents: number,
+  clarity: number,
+  volume: number
+}
+
+export type NoteBlock = {
+  id: number,
+  note: string,
+  midi: number,
+  start: number,
+  end: number,
+  duration: number,
+  confidence: "strong" | "uncertain",
+  averageClarity: number,
+  averageVolume: number
+}
+
+export type CalibrationPoint = {
+  id: number,
+  selectedNote: string,
+  selectedMidi: number,
+  detectedFrequency: number,
+  offsetCents: number,
+}
+
+export type CalibrationChoice = {
+  note: string,
+  midi: number
+}
+
+export type PendingCalibration = {
+  detectedFrequency: number,
+  detectedNote: string,
+  choices: CalibrationChoice[]
+}
